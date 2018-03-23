21557a6e0772790af0d97a11e9e0e26bfbd3df64
blay09 <blay09@hotmail.de>
2018-03-23 13:42:23 +0100

    Fix own particle effects not being hidden completely.
    #12

--- src/main/java/net/blay09/mods/clienttweaks/tweak/HideOwnEffectParticles.java
+++ src/main/java/net/blay09/mods/clienttweaks/tweak/HideOwnEffectParticles.java
@@ -6,22 +6,23 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class HideOwnEffectParticles extends ClientTweak {
 
-	public HideOwnEffectParticles() {
-		super("Hide Own Particle Effects");
-	}
+    public HideOwnEffectParticles() {
+        super("Hide Own Particle Effects");
+    }
 
-	@SubscribeEvent
-	public void onClientTick(TickEvent.ClientTickEvent event) {
-		if(event.phase == TickEvent.Phase.END && isEnabled()) {
-			if (mc.player != null) {
-				mc.player.getDataManager().set(EntityLivingBase.HIDE_PARTICLES, true);
-			}
-		}
-	}
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && isEnabled()) {
+            if (mc.player != null) {
+                mc.player.getDataManager().set(EntityLivingBase.HIDE_PARTICLES, true);
+                mc.player.getDataManager().set(EntityLivingBase.POTION_EFFECTS, 0);
+            }
+        }
+    }
 
-	@Override
-	public String getDescription() {
-		return "This option will hide most of your own potion particle effects for your client (other players will still see them).";
-	}
+    @Override
+    public String getDescription() {
+        return "This option will hide your own potion particle effects for your client (other players will still see them).";
+    }
 
 }
