1fbc751f1d9f078fa5e45106bceb3b4623a5b8e5
blay09 <blay09@hotmail.de>
2021-02-28 15:33:49 +0100

    Fix mod showing as required on server-side
    #39

--- src/main/java/net/blay09/mods/clienttweaks/ClientTweaks.java
+++ src/main/java/net/blay09/mods/clienttweaks/ClientTweaks.java
@@ -3,11 +3,14 @@ package net.blay09.mods.clienttweaks;
 import net.blay09.mods.clienttweaks.tweak.*;
 import net.minecraft.util.SoundCategory;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +26,9 @@ public class ClientTweaks {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientTweaksConfig.clientSpec);
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+
+        // clientSideOnly = true was cooler
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
     private void setupClient(FMLClientSetupEvent event) {
