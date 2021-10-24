9906e88badf44320774bf9a82396dbcb48078bd2
blay09 <blay09@hotmail.de>
2021-10-24 11:04:37 +0200

    prepare for release

--- forge/src/main/java/net/blay09/mods/clienttweaks/ForgeClientTweaks.java
+++ forge/src/main/java/net/blay09/mods/clienttweaks/ForgeClientTweaks.java
@@ -2,13 +2,18 @@ package net.blay09.mods.clienttweaks;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod(ClientTweaks.MOD_ID)
 public class ForgeClientTweaks {
 
     public ForgeClientTweaks() {
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientTweaks::initialize);
+
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }
