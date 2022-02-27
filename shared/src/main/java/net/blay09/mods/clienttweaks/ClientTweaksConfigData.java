bc9fb6c97ae7e646850c6a4e31ac49e9a229ef74
blay09 <blay09@hotmail.de>
2022-02-27 18:42:21 +0100

    Added option for no recipe book shift

--- shared/src/main/java/net/blay09/mods/clienttweaks/ClientTweaksConfigData.java
+++ shared/src/main/java/net/blay09/mods/clienttweaks/ClientTweaksConfigData.java
@@ -64,7 +64,7 @@ public class ClientTweaksConfigData implements BalmConfigData {
         @Comment("This option will make iron fences and glass panes have a bigger hitbox while placing them, making it easier to aim.")
         public boolean paneBuildingSupport = true;
 
-        @Comment("This option makes the recipe book not shift the inventory when opened. Works best with GUI scales below 5.")
+        @Comment("This option makes the recipe book not shift the inventory when opened. Works best with smaller GUI scales / bigger resolutions.")
         public boolean noRecipeBookShifting = false;
     }
 
