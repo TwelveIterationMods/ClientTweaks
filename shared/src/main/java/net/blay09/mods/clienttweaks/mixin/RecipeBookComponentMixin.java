9ea41db8432e0fd2af43bf20b27275c9a5b69097
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-03-16 16:44:21 +0100

    fix: Fix recipe book closing when an item is clicked when noRecipeBookShifting is enabled #51

--- shared/src/main/java/net/blay09/mods/clienttweaks/mixin/RecipeBookComponentMixin.java
+++ shared/src/main/java/net/blay09/mods/clienttweaks/mixin/RecipeBookComponentMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RecipeBookComponent.class)
 public class RecipeBookComponentMixin {
@@ -22,6 +23,13 @@ public class RecipeBookComponentMixin {
         if (ClientTweaksConfig.getActive().tweaks.noRecipeBookShifting) {
             widthTooNarrow = true;
             xOffset = 162;
+        }
+    }
+
+    @Inject(method = "isOffsetNextToMainGUI()Z", at = @At("HEAD"), cancellable = true)
+    void isOffsetNextToMainGUI(CallbackInfoReturnable<Boolean> callbackInfo) {
+        if (ClientTweaksConfig.getActive().tweaks.noRecipeBookShifting) {
+            callbackInfo.setReturnValue(true); // we pretend like we're not shifted to prevent the recipe book from being closed
         }
     }
 
