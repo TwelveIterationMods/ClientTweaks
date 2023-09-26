08fd16c07f61113b865366c22822cb07f4832415
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-09-26 14:19:04 +0200

    fix: Fix compile error on Forge

--- forge/src/main/java/net/blay09/mods/clienttweaks/ForgeClientTweaks.java
+++ forge/src/main/java/net/blay09/mods/clienttweaks/ForgeClientTweaks.java
@@ -7,7 +7,6 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ClientTweaks.MOD_ID)
 public class ForgeClientTweaks {
@@ -16,7 +15,7 @@ public class ForgeClientTweaks {
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> Balm.initialize(ClientTweaks.MOD_ID, ClientTweaks::initializeCommon));
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(ClientTweaks.MOD_ID, ClientTweaks::initializeClient));
 
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }
