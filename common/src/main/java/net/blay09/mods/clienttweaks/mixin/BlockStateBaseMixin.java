d6316ade4085c68e8ca10927d653f8e65ce1d6f2
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-25 08:31:49 +0200

    feat: Add creativeBreakingSupport for blocks with random offsets #80

--- common/src/main/java/net/blay09/mods/clienttweaks/mixin/BlockStateBaseMixin.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/BlockStateBaseMixin.java
@@ -4,8 +4,6 @@ import net.blay09.mods.clienttweaks.ClientTweaksConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.CrossCollisionBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
@@ -17,13 +15,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BlockBehaviour.class)
-public class BlockBehaviourMixin {
-
-    @Inject(method = "getShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"), cancellable = true)
-    void getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> callbackInfo) {
+@Mixin(BlockBehaviour.BlockStateBase.class)
+public class BlockStateBaseMixin {
+    @SuppressWarnings("UnreachableCode")
+    @Inject(method = "getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"), cancellable = true)
+    void getShape(BlockGetter blockGetter, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> callbackInfo) {
+        @SuppressWarnings("DataFlowIssue") final var state = (BlockState) (Object) this;
         final var minecraft = Minecraft.getInstance();
-        @SuppressWarnings("ConstantValue") final var player = minecraft != null ? minecraft.player : null;
+        final var player = minecraft != null ? minecraft.player : null;
         boolean isCreative = player != null && player.getAbilities().instabuild;
         if (isCreative && ClientTweaksConfig.getActive().tweaks.creativeBreakingSupport && state.hasOffsetFunction()) {
             VoxelShape originalShape = callbackInfo.getReturnValue();
@@ -35,5 +34,4 @@ public class BlockBehaviourMixin {
             callbackInfo.setReturnValue(modifiedShape);
         }
     }
-
 }
