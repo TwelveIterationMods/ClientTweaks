d360bef83dae9fcd696b77a36ac1d830b10ed5a5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-05-02 22:12:17 +0200

    fix: Fix disableStepAssist disabling step assist #98

---
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/LivingEntityMixin.java
@@ -0,0 +1,25 @@
+package net.blay09.mods.clienttweaks.mixin;
+
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.clienttweaks.ClientTweaksConfigData;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+    @SuppressWarnings("DataFlowIssue")
+    @Inject(method = "maxUpStep", at = @At("RETURN"), cancellable = true)
+    public void maxUpStep(CallbackInfoReturnable<Float> cir) {
+        if (Balm.getConfig().getActiveConfig(ClientTweaksConfigData.class).tweaks.disableStepAssist) {
+            final var baseMaxStep = ((LivingEntity) (Object) this).getAttributeBaseValue(Attributes.STEP_HEIGHT);
+            final var modifiedMaxStep = cir.getReturnValue();
+            cir.setReturnValue((float) Math.min(baseMaxStep, modifiedMaxStep));
+        }
+    }
+
+}
--- common/src/main/java/net/blay09/mods/clienttweaks/tweak/StepAssistIsAnnoying.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/StepAssistIsAnnoying.java
@@ -14,33 +14,8 @@ import net.minecraft.world.entity.player.Player;
 
 public class StepAssistIsAnnoying extends AbstractClientTweak {
 
-    private final ResourceLocation disableStepAssistModifierId = ResourceLocation.fromNamespaceAndPath("clienttweaks", "disable_step_assist");
-    private final AttributeModifier disableStepAssistModifier = new AttributeModifier(disableStepAssistModifierId,
-            -1,
-            AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
-
     public StepAssistIsAnnoying() {
         super("disableStepAssist");
-
-        // TODO might have to add prio to tick event handlers since this used to run on Lowest
-        Balm.getEvents().onTickEvent(TickType.Client, TickPhase.Start, this::onPlayerTick);
-    }
-
-    public void onPlayerTick(Minecraft client) {
-        Player player = client.player;
-        if (player != null) {
-            if (isEnabled()) {
-                AttributeInstance attributeInstance = player.getAttribute(Attributes.STEP_HEIGHT);
-                if (attributeInstance != null && !attributeInstance.hasModifier(disableStepAssistModifierId)) {
-                    attributeInstance.addTransientModifier(disableStepAssistModifier);
-                }
-            } else {
-                AttributeInstance attributeInstance = player.getAttribute(Attributes.STEP_HEIGHT);
-                if (attributeInstance != null) {
-                    attributeInstance.removeModifier(disableStepAssistModifier.id());
-                }
-            }
-        }
     }
 
     @Override
