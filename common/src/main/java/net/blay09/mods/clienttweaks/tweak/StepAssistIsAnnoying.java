7ef4043e3eab3933538f2f4b7d58406cdcd949c2
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-05-02 22:12:17 +0200

    fix: Fix disableStepAssist disabling step assist #98

--- common/src/main/java/net/blay09/mods/clienttweaks/tweak/StepAssistIsAnnoying.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/StepAssistIsAnnoying.java
@@ -1,44 +1,13 @@
 package net.blay09.mods.clienttweaks.tweak;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.event.TickPhase;
-import net.blay09.mods.balm.api.event.TickType;
 import net.blay09.mods.clienttweaks.ClientTweaksConfig;
 import net.blay09.mods.clienttweaks.ClientTweaksConfigData;
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 
 public class StepAssistIsAnnoying extends AbstractClientTweak {
 
-    private final ResourceLocation disableStepAssistModifierId = ResourceLocation.fromNamespaceAndPath("clienttweaks", "disable_step_assist");
-    private final AttributeModifier disableStepAssistModifier = new AttributeModifier(disableStepAssistModifierId,
-            -1,
-            AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
-
     public StepAssistIsAnnoying() {
-        super("disable_step_assist");
-
-        // TODO might have to add prio to tick event handlers since this used to run on Lowest
-        Balm.getEvents().onTickEvent(TickType.Client, TickPhase.Start, this::onPlayerTick);
-    }
-
-    public void onPlayerTick(Minecraft client) {
-        final var player = client.player;
-        if (player != null) {
-            if (isEnabled()) {
-                final var attributeInstance = player.getAttribute(Attributes.STEP_HEIGHT);
-                if (attributeInstance != null && !attributeInstance.hasModifier(disableStepAssistModifierId)) {
-                    attributeInstance.addTransientModifier(disableStepAssistModifier);
-                }
-            } else {
-                final var attributeInstance = player.getAttribute(Attributes.STEP_HEIGHT);
-                if (attributeInstance != null) {
-                    attributeInstance.removeModifier(disableStepAssistModifier.id());
-                }
-            }
-        }
+        super("disableStepAssist");
     }
 
     @Override
