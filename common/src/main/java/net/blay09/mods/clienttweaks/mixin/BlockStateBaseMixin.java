aa822c3819bccb57b0e0d0a9359f0bfdc5f54e31
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-10-28 09:46:55 +0100

    fix: Fix crash on empty shapes #109

--- common/src/main/java/net/blay09/mods/clienttweaks/mixin/BlockStateBaseMixin.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/BlockStateBaseMixin.java
@@ -25,13 +25,15 @@ public class BlockStateBaseMixin {
         final var player = minecraft != null ? minecraft.player : null;
         boolean isCreative = player != null && player.getAbilities().instabuild;
         if (isCreative && ClientTweaksConfig.getActive().tweaks.creativeBreakingSupport && state.hasOffsetFunction()) {
-            VoxelShape originalShape = callbackInfo.getReturnValue();
-            VoxelShape modifiedShape = Shapes.create(originalShape.bounds()
-                    .expandTowards(-1, 0, -1)
-                    .expandTowards(1, 0, 1)
-                    .intersect(new AABB(0, 0, 0, 1, 1, 1))
-            );
-            callbackInfo.setReturnValue(modifiedShape);
+            final var originalShape = callbackInfo.getReturnValue();
+            if (!originalShape.isEmpty()) {
+                final var modifiedShape = Shapes.create(originalShape.bounds()
+                        .expandTowards(-1, 0, -1)
+                        .expandTowards(1, 0, 1)
+                        .intersect(new AABB(0, 0, 0, 1, 1, 1))
+                );
+                callbackInfo.setReturnValue(modifiedShape);
+            }
         }
     }
 }
--- common/src/main/java/net/blay09/mods/clienttweaks/mixin/ChainBlockMixin.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/ChainBlockMixin.java
@@ -3,11 +3,9 @@ package net.blay09.mods.clienttweaks.mixin;
 import net.blay09.mods.clienttweaks.ClientTweaksConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChainBlock;
-import net.minecraft.world.level.block.CrossCollisionBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -27,13 +25,15 @@ public class ChainBlockMixin {
         @SuppressWarnings("ConstantValue") final var player = minecraft != null ? minecraft.player : null;
         boolean isHoldingChainBlock = player != null && Block.byItem(player.getMainHandItem().getItem()) instanceof ChainBlock;
         if (isHoldingChainBlock && ClientTweaksConfig.getActive().tweaks.chainBuildingSupport) {
-            VoxelShape originalShape = callbackInfo.getReturnValue();
-            VoxelShape modifiedShape = Shapes.create(originalShape.bounds()
-                    .expandTowards(0.25, 0.25, 0.25)
-                    .expandTowards(-0.25, -0.25, -0.25)
-                    .intersect(new AABB(0, 0, 0, 1, 1, 1))
-            );
-            callbackInfo.setReturnValue(modifiedShape);
+            final var originalShape = callbackInfo.getReturnValue();
+            if (!originalShape.isEmpty()) {
+                final var modifiedShape = Shapes.create(originalShape.bounds()
+                        .expandTowards(0.25, 0.25, 0.25)
+                        .expandTowards(-0.25, -0.25, -0.25)
+                        .intersect(new AABB(0, 0, 0, 1, 1, 1))
+                );
+                callbackInfo.setReturnValue(modifiedShape);
+            }
         }
     }
 
