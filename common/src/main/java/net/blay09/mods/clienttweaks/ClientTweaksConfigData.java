f2ddd1acd0d6bd4a6016e31e7dfe803e67680085
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-12-08 17:14:58 +0100

    Update common/src/main/java/net/blay09/mods/clienttweaks/ClientTweaksConfigData.java

--- common/src/main/java/net/blay09/mods/clienttweaks/ClientTweaksConfigData.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/ClientTweaksConfigData.java
@@ -142,7 +142,7 @@ public class ClientTweaksConfigData {
                 ResourceLocation.withDefaultNamespace("large_amethyst_bud")
         );
 
-        @Comment("Items that count as food for the offhand-torchWithFood tweak options.")
+        @Comment("Items that count as food for the offhand-torch and -use tweak options.")
         @NestedType(String.class)
         public List<String> foodItems = Lists.newArrayList(
                 "supplementaries:lunch_basket"
