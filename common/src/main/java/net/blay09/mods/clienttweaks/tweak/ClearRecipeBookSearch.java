705c6a7a3be9d4586c78d5fc8c397e517e598753
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-12-22 08:24:17 +0100

    fix(1.21.11): Fix wrong check for right click button

--- common/src/main/java/net/blay09/mods/clienttweaks/tweak/ClearRecipeBookSearch.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/ClearRecipeBookSearch.java
@@ -1,5 +1,6 @@
 package net.blay09.mods.clienttweaks.tweak;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.client.platform.event.callback.ScreenCallback;
 import net.blay09.mods.clienttweaks.ClientTweaksConfig;
@@ -20,7 +21,7 @@ public class ClearRecipeBookSearch extends AbstractClientTweak {
 
     private boolean onRightClick(Screen screen, MouseButtonEvent event) {
         if (isEnabled()) {
-            if (event.isRight() && screen instanceof AbstractRecipeBookScreen) {
+            if (event.button() == InputConstants.MOUSE_BUTTON_RIGHT && screen instanceof AbstractRecipeBookScreen) {
                 var recipeBookComponent = ((AbstractRecipeBookScreenAccessor) screen).getRecipeBookComponent();
                 var editBox = ((RecipeBookComponentAccessor) recipeBookComponent).getSearchBox();
                 if (editBox.isMouseOver(event.x(), event.y())) {
