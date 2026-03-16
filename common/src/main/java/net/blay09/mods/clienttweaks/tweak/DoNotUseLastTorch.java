428f388ade3c82e8dae66e162b757722bf8a5d98
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-16 10:57:21 +0100

    fix: Run do_not_use_last_torch last to avoid showing Out of Torches when event would cancel anyway #114

--- common/src/main/java/net/blay09/mods/clienttweaks/tweak/DoNotUseLastTorch.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/DoNotUseLastTorch.java
@@ -2,6 +2,7 @@ package net.blay09.mods.clienttweaks.tweak;
 
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.client.platform.event.callback.ClientItemCallback;
+import net.blay09.mods.balm.platform.event.EventPhases;
 import net.blay09.mods.balm.platform.event.callback.InteractionEventResult;
 import net.blay09.mods.clienttweaks.ClientTweaksConfig;
 import net.blay09.mods.clienttweaks.ClientTweaksConfigData;
@@ -18,7 +19,7 @@ public class DoNotUseLastTorch extends AbstractClientTweak {
     public DoNotUseLastTorch() {
         super("do_not_use_last_torch");
 
-        ClientItemCallback.Use.EVENT.register(this::onRightClick);
+        ClientItemCallback.Use.EVENT.register(EventPhases.LOWEST, this::onRightClick);
     }
 
     public InteractionEventResult onRightClick(Player player, InteractionHand hand) {
