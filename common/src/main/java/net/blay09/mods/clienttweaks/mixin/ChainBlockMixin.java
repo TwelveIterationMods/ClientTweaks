59b54892b1d6da75982633dc176b5a217884d25c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-17 14:37:24 +0100

    feat: Expand chainBuildingSupport to also trigger while holding an item tagged chains, as well as while holding any block and standing below #126

--- common/src/main/java/net/blay09/mods/clienttweaks/mixin/ChainBlockMixin.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/ChainBlockMixin.java
@@ -3,6 +3,9 @@ package net.blay09.mods.clienttweaks.mixin;
 import net.blay09.mods.clienttweaks.ClientTweaksConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChainBlock;
@@ -21,10 +24,20 @@ public class ChainBlockMixin {
 
     @Inject(method = "getShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"), cancellable = true)
     void getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> callbackInfo) {
+        final var config = ClientTweaksConfig.getActive();
+        if (config == null || !config.tweaks.chainBuildingSupport) {
+            return;
+        }
+
         final var minecraft = Minecraft.getInstance();
         @SuppressWarnings("ConstantValue") final var player = minecraft != null ? minecraft.player : null;
-        final var isHoldingChainBlock = player != null && Block.byItem(player.getMainHandItem().getItem()) instanceof ChainBlock;
-        if (isHoldingChainBlock && ClientTweaksConfig.getActive().tweaks.chainBuildingSupport) {
+        if (player == null) {
+            return;
+        }
+
+        if (player.getMainHandItem().is(ItemTags.CHAINS)
+                || Block.byItem(player.getMainHandItem().getItem()) instanceof ChainBlock
+                || (player.getBlockY() < pos.getY() - 1 && player.getMainHandItem().getItem() instanceof BlockItem)) {
             final var originalShape = callbackInfo.getReturnValue();
             if (!originalShape.isEmpty()) {
                 final var modifiedShape = Shapes.create(originalShape.bounds()
