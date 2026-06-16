dad0329d314ff077f40cc9a960174f5dbf935076
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-16 19:00:31 +0200

    refactor(26.2): GuiMixin -> HudMixin

--- common/src/main/java/net/blay09/mods/clienttweaks/mixin/HudMixin.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/HudMixin.java
@@ -2,13 +2,14 @@ package net.blay09.mods.clienttweaks.mixin;
 
 import net.blay09.mods.clienttweaks.ClientTweaksConfig;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Gui.class)
-public class GuiMixin {
+@Mixin(Hud.class)
+public class HudMixin {
 
     @Inject(method = "willPrioritizeExperienceInfo", at = @At("HEAD"), cancellable = true)
     private void willPrioritizeExperienceInfo(CallbackInfoReturnable<Boolean> cir) {
