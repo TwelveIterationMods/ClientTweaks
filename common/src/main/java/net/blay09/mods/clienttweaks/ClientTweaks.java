480a123b7b15ab52e1ad841edbb7e5f3dc72b7bf
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-02 16:07:56 +0200

    feat: Backport hideShieldUnlessBlocking tweak

--- common/src/main/java/net/blay09/mods/clienttweaks/ClientTweaks.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/ClientTweaks.java
@@ -55,6 +55,7 @@ public class ClientTweaks {
         registerTweak(new HideEmptyMainHand());
         registerTweak(new StepAssistIsAnnoying());
         registerTweak(new AutoClimbLadder());
+        registerTweak(new HideShieldUnlessBlocking());
         registerTweak(new HideShieldUnlessHoldingWeapon());
         registerTweak(new DoNotUseLastTorch());
         registerTweak(new DisableLogStripping());
