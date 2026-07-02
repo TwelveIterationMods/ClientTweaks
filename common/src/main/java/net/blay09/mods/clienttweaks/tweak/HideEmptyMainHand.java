c08d78a0e86c3356d7c669f722f1e4b64965dd30
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-02 16:04:47 +0200

    feat: Backport hideHands and hideEmptyMainHand tweaks

---
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/HideEmptyMainHand.java
@@ -0,0 +1,33 @@
+package net.blay09.mods.clienttweaks.tweak;
+
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.event.EventPriority;
+import net.blay09.mods.balm.api.event.client.RenderHandEvent;
+import net.blay09.mods.clienttweaks.ClientTweaksConfig;
+import net.blay09.mods.clienttweaks.ClientTweaksConfigData;
+import net.minecraft.world.InteractionHand;
+
+public class HideEmptyMainHand extends AbstractClientTweak {
+
+    public HideEmptyMainHand() {
+        super("hideEmptyMainHand");
+
+        Balm.getEvents().onEvent(RenderHandEvent.class, this::onRenderHand, EventPriority.Highest);
+    }
+
+    public void onRenderHand(RenderHandEvent event) {
+        if (isEnabled() && event.getHand() == InteractionHand.MAIN_HAND && event.getItemStack().isEmpty()) {
+            event.setCanceled(true);
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ClientTweaksConfig.getActive().tweaks.hideEmptyMainHand;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        Balm.getConfig().updateConfig(ClientTweaksConfigData.class, it -> it.tweaks.hideEmptyMainHand = enabled);
+    }
+}
---
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/HideHands.java
@@ -0,0 +1,37 @@
+package net.blay09.mods.clienttweaks.tweak;
+
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.event.EventPriority;
+import net.blay09.mods.balm.api.event.client.RenderHandEvent;
+import net.blay09.mods.clienttweaks.ClientTweaksConfig;
+import net.blay09.mods.clienttweaks.ClientTweaksConfigData;
+
+public class HideHands extends AbstractClientTweak {
+
+    public HideHands() {
+        super("hideHands");
+
+        Balm.getEvents().onEvent(RenderHandEvent.class, this::onRenderHand, EventPriority.Highest);
+    }
+
+    public void onRenderHand(RenderHandEvent event) {
+        if (isEnabled()) {
+            event.setCanceled(true);
+        }
+    }
+
+    @Override
+    public boolean hasKeyBinding() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ClientTweaksConfig.getActive().tweaks.hideHands;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        Balm.getConfig().updateConfig(ClientTweaksConfigData.class, it -> it.tweaks.hideHands = enabled);
+    }
+}
