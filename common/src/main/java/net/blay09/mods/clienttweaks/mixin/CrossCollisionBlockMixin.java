ef0d7af89a95049e99e9d4bf8b2f787c4cf66728
Damir Krupic <61109153+DadoIrie@users.noreply.github.com>
2026-07-27 15:42:50 +0200

    fix: Fix Pane Building Support incorrectly affecting Diagonal Walls / Windows / etc. (#143)
    Co-authored-by: BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
    (cherry picked from commit 7a3cb3afd53c57cc4436e8da46a4383b32499ddc)

--- common/src/main/java/net/blay09/mods/clienttweaks/mixin/CrossCollisionBlockMixin.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/mixin/CrossCollisionBlockMixin.java
@@ -1,15 +1,11 @@
 package net.blay09.mods.clienttweaks.mixin;
 
-import net.blay09.mods.clienttweaks.ClientTweaksConfig;
-import net.minecraft.client.Minecraft;
+import net.blay09.mods.clienttweaks.tweak.PaneBuildingSupport;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CrossCollisionBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,44 +17,10 @@ public class CrossCollisionBlockMixin {
 
     @Inject(method = "getShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"), cancellable = true)
     void getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> callbackInfo) {
-        final var minecraft = Minecraft.getInstance();
-        @SuppressWarnings("ConstantValue") final var player = minecraft != null ? minecraft.player : null;
-        boolean isHoldingCrossCollisionBlock = player != null && Block.byItem(player.getMainHandItem().getItem()) instanceof CrossCollisionBlock;
-        if (isHoldingCrossCollisionBlock && ClientTweaksConfig.getActive().building.paneBuildingSupport) {
-            // Exit out early if the block does not have the properties we use, to prevent crashes with mods that extend CrossCollisionBlock
-            if (!state.hasProperty(CrossCollisionBlock.EAST) || !state.hasProperty(CrossCollisionBlock.WEST) || !state.hasProperty(CrossCollisionBlock.NORTH) || !state.hasProperty(
-                    CrossCollisionBlock.SOUTH)) {
-                return;
-            }
-
-            boolean isPillarSection = !state.getValue(CrossCollisionBlock.EAST) && !state.getValue(CrossCollisionBlock.WEST) && !state.getValue(
-                    CrossCollisionBlock.NORTH) && !state.getValue(CrossCollisionBlock.SOUTH);
-            boolean isThinSection = isOnlyOneTrue(state.getValue(CrossCollisionBlock.EAST),
-                    state.getValue(CrossCollisionBlock.WEST),
-                    state.getValue(CrossCollisionBlock.NORTH),
-                    state.getValue(CrossCollisionBlock.SOUTH));
-            if (isThinSection || isPillarSection) {
-                final var originalShape = callbackInfo.getReturnValue();
-                if (!originalShape.isEmpty()) {
-                    final var modifiedShape = Shapes.create(originalShape.bounds()
-                            .expandTowards(0.25, 0, 0.25)
-                            .expandTowards(-0.25, 0, -0.25)
-                            .intersect(new AABB(0.01, 0.01, 0.01, 0.99, 0.99, 0.99))
-                    );
-                    callbackInfo.setReturnValue(modifiedShape);
-                }
-            }
+        final var shape = PaneBuildingSupport.getShape(state, context, callbackInfo.getReturnValue());
+        if (shape != null) {
+            callbackInfo.setReturnValue(shape);
         }
-    }
-
-    private static boolean isOnlyOneTrue(boolean... args) {
-        int trues = 0;
-        for (boolean arg : args) {
-            if (arg) {
-                trues++;
-            }
-        }
-        return trues == 1;
     }
 
 }
