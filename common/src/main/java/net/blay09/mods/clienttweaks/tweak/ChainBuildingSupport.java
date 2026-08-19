5ce63db2b7f647bc1dbb2f5f6fddb1757d606af0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-19 08:41:44 +0200

    fix: Fix crash when getShape is called with abnormal parameters #149

--- common/src/main/java/net/blay09/mods/clienttweaks/tweak/ChainBuildingSupport.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/ChainBuildingSupport.java
@@ -21,6 +21,10 @@ public class ChainBuildingSupport {
 
     @Nullable
     public static VoxelShape getShape(BlockState state, BlockPos pos, CollisionContext context, VoxelShape originalShape) {
+        if (state == null || pos == null || context == null || originalShape == null) {
+            return null;
+        }
+
         final var minecraft = Minecraft.getInstance();
         @SuppressWarnings("ConstantValue") final var player = minecraft != null ? minecraft.player : null;
         final var isHoldingChainBlock = player != null && (player.getMainHandItem().is(ItemTags.CHAINS) || Block.byItem(player.getMainHandItem().getItem()) instanceof ChainBlock);
