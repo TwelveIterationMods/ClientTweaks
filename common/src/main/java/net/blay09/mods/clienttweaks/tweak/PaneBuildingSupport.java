ebfeac9f6b49af1f563031b1a4a2c3785ac245a6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-19 08:41:44 +0200

    fix: Fix crash when getShape is called with abnormal parameters #149

--- common/src/main/java/net/blay09/mods/clienttweaks/tweak/PaneBuildingSupport.java
+++ common/src/main/java/net/blay09/mods/clienttweaks/tweak/PaneBuildingSupport.java
@@ -18,6 +18,10 @@ public class PaneBuildingSupport {
 
     @Nullable
     public static VoxelShape getShape(BlockState state, CollisionContext context, VoxelShape originalShape) {
+        if (state == null || context == null || originalShape == null) {
+            return null;
+        }
+
         final var minecraft = Minecraft.getInstance();
         @SuppressWarnings("ConstantValue") final var player = minecraft != null ? minecraft.player : null;
         final var isHoldingCrossCollisionBlock = player != null && Block.byItem(player.getMainHandItem().getItem()) instanceof CrossCollisionBlock;
